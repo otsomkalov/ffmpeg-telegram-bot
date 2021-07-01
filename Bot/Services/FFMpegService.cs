b6e516faa1e406499555decaaced2bb50f46455a
Oleg <infinitu1327@gmail.com>
2021-07-01 16:48:23 +0300

    Fix swapped width and height

--- Bot/Services/FFMpegService.cs
+++ Bot/Services/FFMpegService.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Bot.Settings;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Bot.Services
@@ -10,32 +12,46 @@ namespace Bot.Services
     public class FFMpegService
     {
         private readonly FFMpegSettings _settings;
+        private readonly ILogger<FFMpegService> _logger;
 
-        public FFMpegService(IOptions<FFMpegSettings> settings)
+        public FFMpegService(IOptions<FFMpegSettings> settings, ILogger<FFMpegService> logger)
         {
+            _logger = logger;
             _settings = settings.Value;
         }
 
         public async Task<string> ConvertAsync(string filePath)
         {
             var outputFilePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.mp4");
-            
+
+            var argumentsParts = new List<string>
+            {
+                $"-i {filePath}",
+                "-filter:v scale='trunc(iw/2)*2:trunc(ih/2)*2'",
+                "-c:a aac",
+                "-max_muxing_queue_size 1024",
+                outputFilePath
+            };
+
             var processStartInfo = new ProcessStartInfo
             {
-                RedirectStandardInput  = true,
+                RedirectStandardInput = true,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 FileName = _settings.Path,
-                Arguments = $"-i {filePath} -filter:v scale='trunc(ih/2)*2:trunc(iw/2)*2' -c:a aac -max_muxing_queue_size 1024 {outputFilePath}"
+                Arguments = string.Join(' ', argumentsParts)
             };
-            
+
             var process = Process.Start(processStartInfo);
 
             await process.WaitForExitAsync();
 
-            var toEndAsync = await process.StandardError.ReadToEndAsync();
+            var error = await process.StandardError.ReadToEndAsync();
 
-            Console.WriteLine(toEndAsync);
+            if (!string.IsNullOrEmpty(error))
+            {
+                _logger.LogError(error);
+            }
 
             return outputFilePath;
         }
@@ -43,21 +59,20 @@ namespace Bot.Services
         public async Task<string> GetThumbnailAsync(string filePath)
         {
             var thumbnailFilePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.jpg");
-            
+
             var processStartInfo = new ProcessStartInfo
             {
-                RedirectStandardInput  = true,
+                RedirectStandardInput = true,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 FileName = _settings.Path,
                 Arguments = $"-i {filePath} -ss 1 -vframes 1 {thumbnailFilePath}"
             };
 
-
             var process = Process.Start(processStartInfo);
 
             await process.WaitForExitAsync();
-            
+
             return thumbnailFilePath;
         }
     }
