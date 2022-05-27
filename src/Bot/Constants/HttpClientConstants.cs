cfb7f82a6fc569211d4fad605c3653c6a45feb85
Oleg <infinitu1327@gmail.com>
2022-05-27 23:11:53 +0300

    Update

--- src/Bot/Constants/HttpClientConstants.cs
+++ src/Bot/Constants/HttpClientConstants.cs
@@ -3,5 +3,5 @@
 public static class HttpClientConstants
 {
     public const string ChromeUserAgent =
-        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36";
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36";
 }
