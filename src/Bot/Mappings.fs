ce53fcf76e76cab329db7a9d4f537dc2a9cf3b4a
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-09-22 12:19:52 +0300

    Fix

--- src/Bot/Mappings.fs
+++ src/Bot/Mappings.fs
@@ -9,4 +9,4 @@ type Update with
     match this.Type with
     | UpdateType.Message -> Update.Msg this.Message
     | UpdateType.ChannelPost -> Update.Msg this.ChannelPost
-    | _ -> Update.Other
+    | _ -> Update.Other this.Type
