90eda54ab1a3a8cc96d848391a280a2d46a014bb
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2026-04-02 11:20:29 +0300

    more changes

--- deploy/main.tf
+++ deploy/main.tf
@@ -144,7 +144,6 @@ resource "azurerm_linux_function_app" "func-tg-bot" {
   builtin_logging_enabled     = false
 
   site_config {
-    application_insights_connection_string = azurerm_application_insights.appi-tg-bot.connection_string
     app_scale_limit                        = 10
 
     application_stack {
